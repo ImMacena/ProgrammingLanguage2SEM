f7b6bd4034fc95c5ef306a339617b8dde27882b6
ImMacena <guilhermewill92@gmail.com>
2024-09-09 04:51:21 -0300

    working on Chapter 4: end ex3

--- Cap4.c
+++ Cap4.c
@@ -1,15 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-#define ex1
+#define ex3
 
 /*
 
   OBS.: Todos os programa devem ser finalizados pelo usuario.
-
-  1 - Receba via teclado 10 valores inteiros e ordene por ordem crescente assim
-      que sao digitados. Guarde-os em um vetor. Mostre ao final os valores
-      ordenados.
 
   2 - Escreva um programa para receber via teclado em vetores 3 int, 3 long,
       3 unsigned, 3 float e 3 double e imprima-os no video no seguinte formato:
@@ -22,12 +18,6 @@
                   float               double
         int                 long                unsigned
                   float               double
-
-  3 - Escreva um programa que receba um vetor de inteiros com tamanho 10 e o
-      decomponha em dois outros vetores. Um tera´ as componentes de ordem impar
-      e o outro tera´ as componentes de ordem par.
-      Por exemplo, se o vetor dado for v = {3, 5, 6, 8, 1, 4, 2, 3, 7, 9}, o programa
-      deve gerar os vetores u = {3, 6, 1, 2, 7} e w = {5, 8, 4, 3, 9}.
 
   4 - Escreva um programa que receba um vetor de inteiros com tamanho 10 e o
       decomponha em dois outros vetores. Um tera´ as componentes de valor impar
@@ -58,7 +48,7 @@ int menu()
   system("cls");
 
   printf("=====================================\n");
-  printf("  Sum of the digits of a factorial\n");
+  printf("          Sort Vector\n");
   printf("=====================================\n");
   printf("\n");
   printf(" |  1 - Start\n");
@@ -131,6 +121,129 @@ int main()
       }
 
       system("pause");
+    }
+  }
+}
+
+#endif
+
+// ================================================================================
+
+#ifdef ex3
+
+/*
+  3 - Escreva um programa que receba um vetor de inteiros com tamanho 10 e o
+      decomponha em dois outros vetores. Um tera´ as componentes de ordem impar
+      e o outro tera´ as componentes de ordem par.
+      Por exemplo, se o vetor dado for v = {3, 5, 6, 8, 1, 4, 2, 3, 7, 9}, o programa
+      deve gerar os vetores u = {3, 6, 1, 2, 7} e w = {5, 8, 4, 3, 9}.
+*/
+
+int menu()
+{
+  int opt = 0;
+
+  system("cls");
+
+  printf("=====================================\n");
+  printf("          Decompose vector\n");
+  printf("=====================================\n");
+  printf("\n");
+  printf(" |  1 - Start\n");
+  printf(" |  2 - Exit the program\n");
+  printf("\nSelect an option: ");
+
+  scanf("%d", &opt);
+
+  return opt;
+}
+
+int *getEvenIndexes(int *vector, int length, int *evenCount)
+{
+  *evenCount = (length + 1) / 2; // Calcula o número de elementos pares
+
+  int *response = (int *)malloc(*evenCount * sizeof(int));
+
+  int counter = 0;
+  for (int i = 0; i < length; i++)
+  {
+    if (i % 2 == 0)
+    {
+      response[counter] = vector[i];
+      counter++;
+    }
+  }
+
+  return response;
+}
+
+int *getOddIndexes(int *vector, int length, int *oddCount)
+{
+  *oddCount = length / 2; // Calcula o número de elementos ímpares
+
+  int *response = (int *)malloc(*oddCount * sizeof(int));
+
+  int counter = 0;
+  for (int i = 0; i < length; i++)
+  {
+    if (i % 2 == 1)
+    {
+      response[counter] = vector[i];
+      counter++;
+    }
+  }
+
+  return response;
+}
+
+int main()
+{
+  int optMenu, vectorLength = 10, evenCount, oddCount;
+  int numbers[10];
+  int *vectorEvenIndexes, *vectorOddIndexes;
+
+  while ((optMenu = menu()) != 2)
+  {
+    system("cls");
+
+    if (optMenu == 1)
+    {
+      for (int i = 0; i < vectorLength; i++)
+      {
+        printf("Enter the %do value: ", i + 1);
+        scanf("%d", &numbers[i]);
+      }
+
+      system("cls");
+
+      vectorEvenIndexes = getEvenIndexes(numbers, vectorLength, &evenCount);
+      vectorOddIndexes = getOddIndexes(numbers, vectorLength, &oddCount);
+
+      printf("Original vector: [ ");
+      for (int i = 0; i < vectorLength; i++)
+      {
+        printf("%d ", numbers[i]);
+      }
+      printf("]\n");
+
+      printf("Even indexes vector: [ ");
+      for (int i = 0; i < evenCount; i++)
+      {
+        printf("%d ", vectorEvenIndexes[i]);
+      }
+      printf("]\n");
+
+      printf("Odd indexes vector: [ ");
+      for (int i = 0; i < oddCount; i++)
+      {
+        printf("%d ", vectorOddIndexes[i]);
+      }
+      printf("]\n \n");
+
+      system("pause");
+
+      free(vectorEvenIndexes);
+      free(vectorOddIndexes);
     }
   }
 }
